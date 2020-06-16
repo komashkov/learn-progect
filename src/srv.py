b3498de9e610d2f2c739c0e16f82849ffdb05ae1
komashkov <komashkov41@gmail.com>
2020-06-16 22:41:50 +0300

    anonymous, name, age

--- src/srv.py
+++ src/srv.py
@@ -1,67 +1,89 @@
-import socketserver
 import os
+import socketserver
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 from datetime import datetime
 
 PORT = int(os.getenv("PORT", 8000))
-print(f"port = {PORT}")
+print(f"PORT = {PORT}")
+now = datetime.now().year
+
 
 def get_page(query):
     path, qs = query.split("?") if '?' in query else [query, ""]
-    paths = {
-        '/hello': hello,
-        '/goodbye': goodbye,
-        }
-    return paths[path](qs)
+    switcher = {
+        "/hello": page_hello,
+        "/goodbye": page_goodbye, }
+    return switcher[path](qs) if path in switcher else "no information"
 
-def hello(qs):
-    print(qs)
 
-    qs = parse_qs(qs)
-    print("get_name: ")
-    print(qs)
+def page_hello(qs):
+    qs = parse_qs(qs) if qs != "" else ""
+    name = get_name(qs)
+    year = get_year(qs)
     return f"""
-                    Hello {get_name(qs)}!
-                    You were born in {get_year(qs)} year
-                    Your path: /hello
-                     """
+          Hello {name}! 
+          You were born in {year}.
+        """
 
-def goodbye(qs):
-    print(qs)
-    hour = datetime.now().hour
-    if hour < 6:
-        return f"Good night!"
-    elif hour < 12:
-        return f"Good morning!"
-    elif hour < 18:
-        return f"Good afternoon!"
-    elif hour < 23:
-        return f"Good evening!"
-    else:
-        return f"Good night!"
+
+def page_goodbye(qs):
+    return f"""
+        {get_bye}
+            """
+
 
 def get_name(qs):
-    if 'name' in qs:
-        print("get_name: ")
-        print(qs)
-        return qs["name"][0]
+    if qs == "":
+        return "anonymous"
     else:
-        return "Anonymous"
+        qs = parse_qs(qs)
+        if "name" not in qs:
+            return "anonymous"
+        else:
+            return qs["name"][0]
+
 
 def get_year(qs):
-    if 'age' in qs:
-        print("get age: ")
-        print(qs)
-        return str(datetime.now().year - int(qs['age'][0]))
+    if qs == "":
+        return "no information"
     else:
-        return "I don't know your age:("
+        qs = parse_qs(qs)
+        if "age" not in qs:
+            return "no information"
+        else:
+            today = datetime.today().year
+            return str(today - int(qs["age"][0]))
+
+
+def get_bye(hour):
+    hour = datetime.now().hour
+    if hour < 6:
+        return "Good night"
+    elif hour < 12:
+        return "Good morning"
+    elif hour < 18:
+        return "Good afternoon"
+    elif hour < 23:
+        return "Good evening"
+    else:
+        return "Good night"
 
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        msg = get_page(self.path)
+        if self.path != "":
+            msg = get_page(self.path)
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-length", str(len(msg)))
+            self.end_headers()
+            self.wfile.write(msg.encode())
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
+
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print("it works")
+    print("it" + " works")
     httpd.serve_forever()
