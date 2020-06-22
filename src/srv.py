1e0dacd47c7f21127128523867196a1dad9f07cd
komashkov <komashkov41@gmail.com>
2020-06-22 23:04:26 +0300

    change server

--- src/srv.py
+++ src/srv.py
@@ -3,23 +3,21 @@ import socketserver
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 from datetime import datetime
+from pathlib import Path
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT = {PORT}")
 now = datetime.now().year
 
+PAGES_DIR = Path(__file__).parent.parent.resolve()
+print(f"PAGES_DIR = {PAGES_DIR}")
 
-def get_page(self):
-    path, qs = self.path.split("?") if '?' in self.path else [self.path, ""]
-    switcher = {
-        "/hello": get_page_hello,
-        "/goodbye": get_page_goodbye,
-        #"/pages": get_page_about_me,
-       # "/pages/hobby": get_page_hobby,
-       # "/pages/job": get_page_hobby,
-        #"/pages/education": get_page_education,
-    }
-    return switcher[path](qs) if path in switcher else "no information"
+HOBBY_DIR = PAGES_DIR / "templates"
+print(f"HOBBY_DIR = {HOBBY_DIR}")
+
+
+class NotFound(Exception):
+    pass
 
 def get_page_hello(qs):
     if qs != "":
@@ -30,16 +28,23 @@ def get_page_hello(qs):
           Hello {name}! 
           You were born in {year}.
         """
-    self.respond(msg)
+    self.response(msg)
 
 
 def get_page_goodbye(qs):
-    if qs != "":
-        qs = parse_qs(qs)
-    return msg = f"""
-        {time}
-            """
-    self.respond(msg)
+    hour = datetime.now().hour
+    if hour < 6:
+        return f"Good night"
+    elif hour < 12:
+        return f"Good morning"
+    elif hour < 18:
+        return f"Good afternoon"
+    elif hour < 23:
+        return f"Good evening"
+    else:
+        return f"Good night"
+    self.response(msg)
+
 
 def get_name(qs):
     if qs == "":
@@ -50,6 +55,7 @@ def get_name(qs):
             return "anonymous"
         else:
             return qs["name"][0]
+
 
 def get_year(qs):
     if qs == "":
@@ -62,24 +68,44 @@ def get_year(qs):
             today = datetime.today().year
             return str(today - int(qs["age"][0]))
 
-#def resume(qs):
-    msg = """<html>
-    <head>
-    <title>Main page</title>
-    </head>
-    <body>
-    <h1>Hi! This is my page.</h1>
-    <h2>Links
-    """
+
+def get_page_about_me(self):
+    html = PAGES_DIR / "pages" / "index.html"
+    contents = self.get_file_contents(html)
+    self.response(contents, "text/html")
+
+
+def get_page_hobby(self):
+    html = PAGES_DIR / "pages" / "hobby" / "index.html"
+    contents = self.get_file_contents(html)
+    self.response(contents, "text/html")
+
+
+def get_file_contents(self, fp: Path):
+    if not fp.is_file():
+        raise NotFound()
+
+    with fp.open("r") as src:
+        ct = src.read()
+
+    return ct
+
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path != "":
-            msg = get_page(self.path)
+        path, qs = self.path.split("?") if '?' in self.path else [self.path, ""]
+        switcher = {
+            "/hello": get_page_hello,
+            "/goodbye": get_page_goodbye,
+            "/pages": get_page_about_me,
+            "/pages/hobby": get_page_hobby,
+        }
+        if path in switcher:
+            msg = switcher[path](qs)
 
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-length", str(len(msg)))
+            self.send_header("content-type", "text/plain")
+            self.send_header("content-length", str(len(msg)))
             self.end_headers()
             self.wfile.write(msg.encode())
         else:
